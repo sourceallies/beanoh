528cd04899549bcc834e0554815174b3f998008a
dkessler <dkessler@sourceallies.com>
2011-05-04 12:12:35 -0500

    added test cases for duplicate beans, display bean name in error message instead of the name person all of the time

---
+++ src/test/java/com/sourceallies/beanoh/duplicate/LoadSameContextTwiceTest.java
@@ -0,0 +1,43 @@
+/*
+Copyright (c) 2011  Source Allies
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3.0.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, please visit 
+http://www.gnu.org/licenses/lgpl-3.0.txt.
+ */
+
+package com.sourceallies.beanoh.duplicate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import com.sourceallies.beanoh.BeanohTestCase;
+import com.sourceallies.beanoh.exception.DuplicateBeanDefinitionException;
+
+public class LoadSameContextTwiceTest extends BeanohTestCase {
+
+	@Test
+	public void testDups() {
+		try {
+			assertUniqueBeanContextLoading();
+			fail();
+		} catch (DuplicateBeanDefinitionException e) {
+			assertEquals(
+					"Bean 'person' was defined 2 times:\n\n"
+							+ "class path resource [com/sourceallies/beanoh/duplicate/FirstDuplicate-context.xml]\n"
+							+ "class path resource [com/sourceallies/beanoh/duplicate/FirstDuplicate-context.xml]",
+					e.getMessage());
+		}
+	}
+}
---
+++ src/test/java/com/sourceallies/beanoh/duplicate/UniqueBeanTest.java
@@ -0,0 +1,30 @@
+/*
+Copyright (c) 2011  Source Allies
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation version 3.0.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, please visit 
+http://www.gnu.org/licenses/lgpl-3.0.txt.
+ */
+
+package com.sourceallies.beanoh.duplicate;
+
+import org.junit.Test;
+
+import com.sourceallies.beanoh.BeanohTestCase;
+
+public class UniqueBeanTest extends BeanohTestCase {
+
+	@Test
+	public void testDups() {
+		assertUniqueBeanContextLoading();
+	}
+}
